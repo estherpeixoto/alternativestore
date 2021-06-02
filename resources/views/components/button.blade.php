79a6b0bd05324279b8a4c38d09349fe2deb4958e
Esther Peixoto <37677602+estherpeixoto@users.noreply.github.com>
2021-06-02 17:49:04 -0300

    feat: novas cores para os botões

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,19 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+@props(['color' => 'dark'])
+
+@php
+switch ($color) {
+    case 'blue':
+        $btnColor = 'bg-blue-500 hover:bg-blue-600 active:bg-blue-600 focus:border-blue-600';
+        break;
+    case 'red':
+        $btnColor = 'bg-red-500 hover:bg-red-700 active:bg-red-900 focus:border-red-900';
+        break;
+    default:
+        $btnColor = 'bg-gray-800 hover:bg-gray-700 active:bg-gray-900 focus:border-gray-900';
+        break;
+}
+@endphp
+
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'w-full sm:w-auto inline-flex items-center justify-center px-4 py-4 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:ring focus:outline-none disabled:opacity-25 transition ease-in-out duration-150 ' . $btnColor]) }}>
     {{ $slot }}
 </button>
