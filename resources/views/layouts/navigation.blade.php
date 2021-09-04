832a0c0e5e72a6280935a0e3773ff9b09cdd2613
Esther Peixoto <estherpeixoto13@gmail.com>
2021-09-04 19:28:24 -0300

    feat(dashboard): adicionar botão 'voltar para a loja'

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -61,6 +61,13 @@
 			</button>
 		</div>
 
+		<x-responsive-nav-link :href="route('home')">
+			<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+			  </svg>
+			{{ __('Voltar para a loja') }}
+		</x-responsive-nav-link>
+
 		<x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
 			<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
