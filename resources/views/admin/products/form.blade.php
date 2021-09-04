bd538d8e849d5b0c6030ce96ede13ecadaa10119
Esther Peixoto <estherpeixoto13@gmail.com>
2021-09-04 19:27:49 -0300

    fix(crud produtos): verificar se variável está definida

--- resources/views/admin/products/form.blade.php
+++ resources/views/admin/products/form.blade.php
@@ -11,23 +11,29 @@
         @csrf
 
         @if (isset($action))
-        @if ($action == 'alterar')
-        @method('PUT')
-        @elseif ($action == 'excluir')
-        @method('DELETE')
-        @endif
+            @if ($action == 'alterar')
+                @method('PUT')
+            @elseif ($action == 'excluir')
+                @method('DELETE')
+            @endif
         @endif
 
         <div class="flex items-center justify-center mb-5">
-            <button type="button" @click="geral = true; imagens = false; tamanhos = false;" :class="{'border-b border-gray-700 text-gray-800': geral, 'hover:text-gray-800 text-gray-400 transition': !geral}" class="py-3 text-sm font-semibold uppercase rounded-none focus:outline-none">
+            <button type="button" @click="geral = true; imagens = false; tamanhos = false;"
+                :class="{'border-b border-gray-700 text-gray-800': geral, 'hover:text-gray-800 text-gray-400 transition': !geral}"
+                class="py-3 text-sm font-semibold uppercase rounded-none focus:outline-none">
                 Geral
             </button>
 
-            <button type="button" @click="geral = false; imagens = true; tamanhos = false;" :class="{'border-b border-gray-700 text-gray-800': imagens, 'hover:text-gray-800 text-gray-400 transition': !imagens}" class="py-3 ml-8 text-sm font-semibold uppercase rounded-none focus:outline-none">
+            <button type="button" @click="geral = false; imagens = true; tamanhos = false;"
+                :class="{'border-b border-gray-700 text-gray-800': imagens, 'hover:text-gray-800 text-gray-400 transition': !imagens}"
+                class="py-3 ml-8 text-sm font-semibold uppercase rounded-none focus:outline-none">
                 Imagens
             </button>
 
-            <button type="button" @click="geral = false; imagens = false; tamanhos = true;" :class="{'border-b border-gray-700 text-gray-800': tamanhos, 'hover:text-gray-800 text-gray-400 transition': !tamanhos}" class="py-3 ml-8 text-sm font-semibold uppercase rounded-none focus:outline-none">
+            <button type="button" @click="geral = false; imagens = false; tamanhos = true;"
+                :class="{'border-b border-gray-700 text-gray-800': tamanhos, 'hover:text-gray-800 text-gray-400 transition': !tamanhos}"
+                class="py-3 ml-8 text-sm font-semibold uppercase rounded-none focus:outline-none">
                 Tamanhos
             </button>
         </div>
@@ -52,7 +58,7 @@
 
                 <x-select id="category_id" name="category_id" required>
                     @foreach ($categories as $category)
-                    <option value="{{ $category->id }}">{{ $category->description }}</option>
+                        <option value="{{ $category->id }}">{{ $category->description }}</option>
                     @endforeach
                 </x-select>
             </div>
@@ -60,7 +66,7 @@
             <div class="mt-5">
                 <x-label for="price" :value="__('Preço')" />
 
-                <x-input id="price" type="text" name="price" value="{{ old('price') ?? (number_format($product->price, 2, '.', ',') ?? '') }}" required />
+                <x-input id="price" type="text" name="price" value="{{ old('price') ?? (number_format(($product->price ?? 0), 2, '.', ',') ?? '') }}" required />
             </div>
 
             <div class="mt-5">
@@ -79,9 +85,9 @@
 
             <div class="flex items-center justify-end mt-5">
                 @if (isset($action))
-                <x-button type="submit" color="outline-gray" class="mr-3">
-                    Alterar
-                </x-button>
+                    <x-button type="submit" color="outline-gray" class="mr-3">
+                        Alterar
+                    </x-button>
                 @endif
 
                 <x-button type="button" color="blue" @click="geral = ! geral; imagens = ! imagens;">
@@ -94,7 +100,7 @@
             <div>
                 <x-label for="images" :value="__('Upload de imagens')" />
 
-                <input id="images" name="images[]" type="file" accept="image/*" multiple {{!isset($action) ? 'required' : ''}} class="hidden" />
+                <input id="images" name="images[]" type="file" accept="image/*" multiple {{ !isset($action) ? 'required' : '' }} class="hidden" />
 
                 <div class='w-full'>
                     <div onclick="selectFiles()" class="py-16 mb-2 border-2 border-blue-300 border-dashed rounded-md cursor-pointer" style="background-color: #FBFDFF;">
@@ -105,9 +111,9 @@
 
                         <div id='img-preview' class="grid grid-cols-1 gap-2 px-16 sm:grid-cols-2 md:grid-cols-4">
                             @if (isset($images))
-                            @foreach ($images as $k => $image)
-                            <img src="{{asset("storage/products/$image->filename")}}" alt="img_{{$k}}" />
-                            @endforeach
+                                @foreach ($images as $k => $image)
+                                    <img src="{{ asset("storage/products/$image->filename") }}" alt="img_{{ $k }}" />
+                                @endforeach
                             @endif
                         </div>
                     </div>
@@ -118,7 +124,7 @@
 
             <div class="flex items-center justify-end mt-5">
                 @if (isset($action))
-                <x-button type="submit" color="outline-gray" class="mr-3">Alterar imagens</x-button>
+                    <x-button type="submit" color="outline-gray" class="mr-3">Alterar imagens</x-button>
                 @endif
 
                 <x-button type="button" color="blue" @click="imagens = ! imagens; tamanhos = ! tamanhos;">Continuar</x-button>
@@ -134,7 +140,7 @@
                         <tr class='text-sm leading-normal text-gray-600 uppercase bg-gray-200'>
                             <th class='px-6 py-3 text-left'>#</th>
                             @foreach ($sizes as $size)
-                            <th class='px-6 py-3'>{{ $size->description }}</th>
+                                <th class='px-6 py-3'>{{ $size->description }}</th>
                             @endforeach
                         </tr>
                     </thead>
@@ -144,9 +150,9 @@
                             <th class='px-6 py-3 text-left'>Busto</th>
 
                             @foreach ($sizes as $key => $size)
-                            <td class="text-center">
-                                <input class="py-2 text-center focus:outline-none" name="sizes[{{ $size->id }}][chest][]" value='{{ $productSizes[$key]->chest ?? '0,0' }}' />
-                            </td>
+                                <td class="text-center">
+                                    <input class="py-2 text-center focus:outline-none" name="sizes[{{ $size->id }}][chest][]" value='{{ $productSizes[$key]->chest ?? '0,0' }}' />
+                                </td>
                             @endforeach
                         </tr>
 
@@ -154,9 +160,9 @@
                             <th class='px-6 py-3 text-left'>Ombros</th>
 
                             @foreach ($sizes as $key => $size)
-                            <td class="text-center">
-                                <input class="py-2 text-center focus:outline-none" name="sizes[{{ $size->id }}][shoulder][]" value='{{ $productSizes[$key]->shoulder ?? '0,0' }}' />
-                            </td>
+                                <td class="text-center">
+                                    <input class="py-2 text-center focus:outline-none" name="sizes[{{ $size->id }}][shoulder][]" value='{{ $productSizes[$key]->shoulder ?? '0,0' }}' />
+                                </td>
                             @endforeach
                         </tr>
 
@@ -164,9 +170,9 @@
                             <th class='px-6 py-3 text-left'>Manga</th>
 
                             @foreach ($sizes as $key => $size)
-                            <td class="text-center">
-                                <input class="py-2 text-center focus:outline-none" name="sizes[{{ $size->id }}][sleeve][]" value='{{ $productSizes[$key]->sleeve ?? '0,0' }}' />
-                            </td>
+                                <td class="text-center">
+                                    <input class="py-2 text-center focus:outline-none" name="sizes[{{ $size->id }}][sleeve][]" value='{{ $productSizes[$key]->sleeve ?? '0,0' }}' />
+                                </td>
                             @endforeach
                         </tr>
 
@@ -174,9 +180,9 @@
                             <th class='px-6 py-3 text-left'>Comprimento</th>
 
                             @foreach ($sizes as $key => $size)
-                            <td class="text-center">
-                                <input class="py-2 text-center focus:outline-none" name="sizes[{{ $size->id }}][length][]" value='{{ $productSizes[$key]->length ?? '0,0' }}' />
-                            </td>
+                                <td class="text-center">
+                                    <input class="py-2 text-center focus:outline-none" name="sizes[{{ $size->id }}][length][]" value='{{ $productSizes[$key]->length ?? '0,0' }}' />
+                                </td>
                             @endforeach
                         </tr>
 
@@ -184,9 +190,9 @@
                             <th class='px-6 py-3 text-left'>Cintura</th>
 
                             @foreach ($sizes as $key => $size)
-                            <td class="text-center">
-                                <input class="py-2 text-center focus:outline-none" name="sizes[{{ $size->id }}][waist][]" value='{{ $productSizes[$key]->waist ?? '0,0' }}' />
-                            </td>
+                                <td class="text-center">
+                                    <input class="py-2 text-center focus:outline-none" name="sizes[{{ $size->id }}][waist][]" value='{{ $productSizes[$key]->waist ?? '0,0' }}' />
+                                </td>
                             @endforeach
                         </tr>
                     </tbody>
@@ -208,8 +214,8 @@
     let price = document.getElementById('price');
 
     Inputmask({
-        mask: '999.999,99'
-        , numericInput: true
+        mask: '999.999,99',
+        numericInput: true
     }).mask(price);
 
     const imgPreview = document.getElementById('img-preview')
@@ -231,5 +237,4 @@
             })
         })
     })
-
 </script>
