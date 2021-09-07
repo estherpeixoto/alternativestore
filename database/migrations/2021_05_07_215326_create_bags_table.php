756646b0a4a4aea5772b67fdfdd20a908e20d9b3
Esther Peixoto <estherpeixoto13@gmail.com>
2021-09-07 14:21:06 -0300

    fix(migrations): remover id da bags

--- database/migrations/2021_05_07_215326_create_bags_table.php
+++ database/migrations/2021_05_07_215326_create_bags_table.php
@@ -14,7 +14,6 @@ class CreateBagsTable extends Migration
     public function up()
     {
         Schema::create('bags', function (Blueprint $table) {
-            $table->id();
 			$table->foreignId('user_id')
 				->constrained('users')
 				->onUpdate('cascade')
