52bc0f7c98def256e9b1be09a442140a83681b0a
Esther Peixoto <estherpeixoto13@gmail.com>
2021-11-11 21:56:28 -0300

    fix(Sacola): buscar resumo do pedido

--- app/Models/Bag.php
+++ app/Models/Bag.php
@@ -73,11 +73,11 @@ class Bag extends Model
 			->get(['price_products', 'price_delivery', 'total'])
 			->first();
 
-		$totals->price_products = number_format($totals->price_products, 2, ',', '.');
-		$totals->price_delivery = number_format($totals->price_delivery, 2, ',', '.');
-		$totals->total = number_format($totals->total, 2, ',', '.');
-
-		return $totals;
+		return (object) [
+			'price_products' => number_format($totals->price_products ?? 0, 2, ',', '.'),
+			'price_delivery' => number_format($totals->price_delivery ?? 0, 2, ',', '.'),
+			'total' => number_format($totals->total ?? 0, 2, ',', '.'),
+		];
 	}
 
 	public static function address()
