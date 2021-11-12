250526cb36b67728e14b2e2ff31cbb5081856fca
Esther Peixoto <37677602+estherpeixoto@users.noreply.github.com>
2021-11-12 13:50:07 -0300

    fix: corrigir redirecionamento

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -23,7 +23,11 @@ class RedirectIfAuthenticated
 
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+                return redirect(
+					Auth::user()->type == 'A'
+						? RouteServiceProvider::ADMIN_HOME
+						: RouteServiceProvider::CLIENT_HOME
+				);
             }
         }
 
